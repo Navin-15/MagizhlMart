08c58e9860b0f7ce092dc09a462c11737f17b2ec
Navin-15 <naveenidrish203@gmail.com>
2025-11-23 16:07:14 +0530

    vecrel env commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const userRoutes = require("./routes/userRoutes");
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(express.json());
+
+// // Routes
+// app.use("/api/users", userRoutes);
+
+// // MongoDB connection
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/MagizhlMart", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error(err));
+
+// // Start server
+// const PORT = 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -16,7 +46,7 @@ app.use("/api/users", userRoutes);
 
 // MongoDB connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/MagizhlMart", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -24,5 +54,5 @@ mongoose
   .catch((err) => console.error(err));
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
